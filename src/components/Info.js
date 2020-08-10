d17b2fefcf36194667dba8ade6ba34c4086ba390
Parth <kparth2010@gmail.com>
2020-08-10 20:15:08 +0530

    made changes

--- src/components/Info.js
+++ src/components/Info.js
@@ -11,13 +11,13 @@ const Info = ()=>(
         <div className="info-det">
         Hola! I'm Parth Khandelwal and I'm a full-stack web developer currently living in Dwarka, New Delhi. 
         I try to make responsive and dynamic websites using html,css,javascript and python
-        EDUCATION
+        
         Currently pursuing Btech in Information Technology from University School Of Information 
         Communication And Technology (USICT), Dwarka, GGSIPU, New Delhi
         Graduated from Mount Carmel School, Dwarka in 2018
-        HOBBIES
+       
         I like to read, play games, listen to music and watch a lot of football
-        Want to know more? View More
+       
         </div>
         <div className="info-skills">
         <div className="skill-text">
